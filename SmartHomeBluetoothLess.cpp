0621a012ba6ff6c7e1d8e30fd149f4f1ffbe567f
first_HUNAIN last_SHAMSI <hunainali99@vt.edu>
2020-11-29 20:31:20 +0400

    Main new

---
+++ SmartHomeBluetoothLess.cpp
@@ -0,0 +1,27 @@
+// Libraries
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Fonts/FreeSerif12pt7b.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define OLED_RESET -1
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+
+
+void setup() {
+ 
+Serial.begin(9600);
+
+}
+
+void loop() {
+
+}
+
